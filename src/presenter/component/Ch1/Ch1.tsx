c225e697499fd4ecef6c4730af71accb50fe6e46
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-04-19 22:19:43 +0900

    study

--- src/presenter/component/Ch1/Ch1.tsx
+++ src/presenter/component/Ch1/Ch1.tsx
@@ -1,4 +1,4 @@
-import * as S from "./styles";
+import * as S from "./styles2";
 
 export const Ch1: React.FC = () => (
   <>
--- src/presenter/component/Ch1/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Box = styled.div`
-  box-sizing: border-box;
-  width: 100px;
-  height: 100px;
-  background-color: green;
-  border: solid 3px red;
-`;
