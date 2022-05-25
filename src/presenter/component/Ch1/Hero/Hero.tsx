ca2629d7d478e2939a42db2338e92b223f563c29
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-05-25 19:19:35 +0900

    refactor Hero

--- src/presenter/component/Ch1/Hero/Hero.tsx
+++ src/presenter/component/Ch1/Hero/Hero.tsx
@@ -1,14 +1,36 @@
 import * as S from "./styles";
 
-export const Hero: React.FC = () => (
-  <S.Hero>
-    <S.Catch>
+import { useMedia } from "~/presenter/hooks/useMedia";
+
+export const Hero: React.FC = () =>
+  useMedia().media === "pc" ? <Pc /> : <Sp />;
+
+const Sp: React.FC = () => (
+  <S.HeroForSp>
+    <S.CatchForSp>
       <S.TitleWrapper>
         <h1>
           <S.Img src="/hero.svg" alt="BB Assistant Company" />
         </h1>
-        <S.Description>縁の下の力持ちとしてお仕事を支えます</S.Description>
+        <S.DescriptionForSp>
+          縁の下の力持ちとしてお仕事を支えます
+        </S.DescriptionForSp>
       </S.TitleWrapper>
-    </S.Catch>
-  </S.Hero>
+    </S.CatchForSp>
+  </S.HeroForSp>
+);
+
+const Pc: React.FC = () => (
+  <S.HeroForPc>
+    <S.CatchForPc>
+      <S.TitleWrapper>
+        <h1>
+          <S.Img src="/hero.svg" alt="BB Assistant Company" />
+        </h1>
+        <S.DescriptionForPc>
+          縁の下の力持ちとしてお仕事を支えます
+        </S.DescriptionForPc>
+      </S.TitleWrapper>
+    </S.CatchForPc>
+  </S.HeroForPc>
 );
--- src/presenter/component/Ch1/Hero/styles.ts
+++ src/presenter/component/Ch1/Hero/styles.ts
@@ -1,16 +1,20 @@
 import styled from "styled-components";
 import * as s from "~/presenter/defined/styles";
 
-export const Hero = styled.section`
+export const HeroForSp = styled.section`
   height: 400px;
   background-image: url("/hero.jpg");
   background-position: center;
   background-size: cover;
   display: flex;
+`;
 
-  @media ${s.device.pc} {
-    height: 700px;
-  }
+export const HeroForPc = styled.section`
+  height: 700px;
+  background-image: url("/hero.jpg");
+  background-position: center;
+  background-size: cover;
+  display: flex;
 `;
 
 export const Img = styled.img`
@@ -19,34 +23,37 @@ export const Img = styled.img`
   vertical-align: bottom;
 `;
 
-export const Catch = styled.div`
+export const CatchForSp = styled.div`
   background-color: rgba(255, 255, 255, 0.76);
   flex: auto;
   padding: 33px 0;
   align-self: flex-end;
+`;
 
-  @media ${s.device.pc} {
-    padding: 47px 0 44px 0;
-  }
+export const CatchForPc = styled.div`
+  background-color: rgba(255, 255, 255, 0.76);
+  flex: auto;
+  padding: 47px 0 44px 0;
+  align-self: flex-end;
 `;
 
 export const TitleWrapper = styled.div`
   width: 84%;
   margin: 0 auto;
-
   display: flex;
   flex-direction: column-reverse;
   align-items: center;
   line-height: 1.3;
 `;
 
-export const Description = styled.p`
+export const DescriptionForSp = styled.p`
   font-size: 14px;
   color: ${s.colors.blue};
   margin-bottom: 15px;
+`;
 
-  @media ${s.device.pc} {
-    font-size: 16px;
-    margin-bottom: 22px;
-  }
+export const DescriptionForPc = styled.p`
+  font-size: 16px;
+  color: ${s.colors.blue};
+  margin-bottom: 22px;
 `;
