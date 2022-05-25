11b78128b33c9d4522fcbbc597d521ecda62454e
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-05-25 19:15:29 +0900

    refactor Message

--- src/presenter/component/Ch1/Message/Message.tsx
+++ src/presenter/component/Ch1/Message/Message.tsx
@@ -1,12 +1,31 @@
 import * as S from "./styles";
+import { useMedia } from "~/presenter/hooks/useMedia";
 
-export const Message: React.FC = () => (
-  <S.Message>
+export const Message: React.FC = () =>
+  useMedia().media === "pc" ? <Pc /> : <Sp />;
+
+const Sp: React.FC = () => (
+  <S.MessageForSp>
     <S.Wrapper>
-      <S.Title>ビジネスアシスタントとしてサポートを続けて20年</S.Title>
-      <S.Description>
+      <S.TitleForSP>
+        ビジネスアシスタントとしてサポートを続けて20年
+      </S.TitleForSP>
+      <S.DescriptionForSp>
         プロのビジネスアシスタントとして独立後、さまざまなお客様からご依頼をいただき、多様な業務、事業、効率化などのサポートを行ってきました。得意分野が多岐にわたるスタッフが揃っていますので、必要に応じてどのような業務にも対応いたし、雑事に振り回されることなく、重要な案件に取り組んでいただくことを目指しています。
-      </S.Description>
+      </S.DescriptionForSp>
     </S.Wrapper>
-  </S.Message>
+  </S.MessageForSp>
+);
+
+const Pc: React.FC = () => (
+  <S.MessageForPc>
+    <S.Wrapper>
+      <S.TitleForPc>
+        ビジネスアシスタントとしてサポートを続けて20年
+      </S.TitleForPc>
+      <S.DescriptionForPc>
+        プロのビジネスアシスタントとして独立後、さまざまなお客様からご依頼をいただき、多様な業務、事業、効率化などのサポートを行ってきました。得意分野が多岐にわたるスタッフが揃っていますので、必要に応じてどのような業務にも対応いたし、雑事に振り回されることなく、重要な案件に取り組んでいただくことを目指しています。
+      </S.DescriptionForPc>
+    </S.Wrapper>
+  </S.MessageForPc>
 );
--- src/presenter/component/Ch1/Message/styles.ts
+++ src/presenter/component/Ch1/Message/styles.ts
@@ -1,36 +1,44 @@
 import styled from "styled-components";
 import * as s from "~/presenter/defined/styles";
 
-export const Message = styled.section`
+export const MessageForSp = styled.section`
   background-color: ${s.colors.blue};
   padding: 67px 0;
-  @media ${s.device.pc} {
-    padding: 81px 0;
-  }
 `;
 
-export const Title = styled.h2`
+export const MessageForPc = styled.section`
+  background-color: ${s.colors.blue};
+  padding: 81px 0;
+`;
+
+export const TitleForSP = styled.h2`
   color: ${s.colors.white};
   font-size: 24px;
   margin-bottom: 24px;
   line-height: 1.67;
   text-align: center;
-
-  @media ${s.device.pc} {
-    font-size: 36px;
-    line-height: 1.33;
-  }
 `;
 
-export const Description = styled.p`
+export const TitleForPc = styled.h2`
+  color: ${s.colors.white};
+  margin-bottom: 24px;
+  text-align: center;
+  font-size: 36px;
+  line-height: 1.33;
+`;
+
+export const DescriptionForSp = styled.p`
   color: ${s.colors.white};
   font-size: 14px;
   line-height: 1.86;
+`;
 
-  @media ${s.device.pc} {
-    max-width: 640px;
-    margin: auto;
-  }
+export const DescriptionForPc = styled.p`
+  color: ${s.colors.white};
+  font-size: 14px;
+  line-height: 1.86;
+  max-width: 640px;
+  margin: auto;
 `;
 
 export const Wrapper = styled.div`
