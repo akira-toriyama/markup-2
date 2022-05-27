9a77e8d587ad5e2f500b40f8cb9425de7c312700
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-05-27 19:30:06 +0900

    upd Service

--- src/presenter/component/Ch1/Service/Service.tsx
+++ src/presenter/component/Ch1/Service/Service.tsx
@@ -9,6 +9,48 @@ const Sp: React.FC = () => (
     <S.Wrapper>
       <S.TitleForSp>アシスタントサービス</S.TitleForSp>
       <S.Description>私達にできること</S.Description>
+
+      <S.DetailForSp>
+        <S.ArticleForSp>
+          <S.ArticleImgForSp />
+          <S.ArticleTitleForSp>
+            オフィス業務
+            <br />
+            サポート
+          </S.ArticleTitleForSp>
+          <S.ArticleDescriptionForSp>
+            書類管理、電話・メール応対、スケジュール管理、デジタルトランスフォーメーションなど、さまざまなオフィス業務のサポートを行います。
+          </S.ArticleDescriptionForSp>
+          <S.AnchorBtnForSp href="#">OFFICE</S.AnchorBtnForSp>
+        </S.ArticleForSp>
+        <S.ArticleForSp>
+          <S.ArticleImgForSp />
+          <S.ArticleTitleForSp>
+            マネージメント
+            <br />
+            サポート
+          </S.ArticleTitleForSp>
+          <S.ArticleDescriptionForSp>
+            プロジェクトチームの構成メンバーのマネージメント、案件の進捗管理、リーダーのアシスタントサポートなどを行います。
+          </S.ArticleDescriptionForSp>
+          <S.AnchorBtnForSp href="#">MANAGEMENT</S.AnchorBtnForSp>
+        </S.ArticleForSp>
+
+        <S.ArticleForSp>
+          <S.ArticleImgForSp />
+          <S.ArticleTitleForSp>
+            調査・研究
+            <br />
+            サポート
+          </S.ArticleTitleForSp>
+          <S.ArticleDescriptionForSp>
+            各種事業の立ち上げ、運営にかかわる調査・研究のサポートや、特定分野の専門家との提携のお手伝いなどを行います。
+          </S.ArticleDescriptionForSp>
+          <S.AnchorBtnForSp href="#" className="btn">
+            RESEARCH
+          </S.AnchorBtnForSp>
+        </S.ArticleForSp>
+      </S.DetailForSp>
     </S.Wrapper>
   </S.ServiceForSp>
 );
@@ -19,5 +61,47 @@ const Pc: React.FC = () => (
       <S.TitleForPc>アシスタントサービス</S.TitleForPc>
       <S.Description>私達にできること</S.Description>
     </S.Wrapper>
+
+    <S.DetailForPc>
+      <S.ArticleForPc>
+        <S.ArticleImgForPc />
+        <S.ArticleTitleForPc>
+          オフィス業務
+          <br />
+          サポート
+        </S.ArticleTitleForPc>
+        <S.ArticleDescriptionForPc>
+          書類管理、電話・メール応対、スケジュール管理、デジタルトランスフォーメーションなど、さまざまなオフィス業務のサポートを行います。
+        </S.ArticleDescriptionForPc>
+        <S.AnchorBtnForPc href="#">OFFICE</S.AnchorBtnForPc>
+      </S.ArticleForPc>
+      <S.ArticleForPc>
+        <S.ArticleImgForPc />
+        <S.ArticleTitleForPc>
+          マネージメント
+          <br />
+          サポート
+        </S.ArticleTitleForPc>
+        <S.ArticleDescriptionForPc>
+          プロジェクトチームの構成メンバーのマネージメント、案件の進捗管理、リーダーのアシスタントサポートなどを行います。
+        </S.ArticleDescriptionForPc>
+        <S.AnchorBtnForPc href="#">MANAGEMENT</S.AnchorBtnForPc>
+      </S.ArticleForPc>
+
+      <S.ArticleForPc>
+        <S.ArticleImgForPc />
+        <S.ArticleTitleForPc>
+          調査・研究
+          <br />
+          サポート
+        </S.ArticleTitleForPc>
+        <S.ArticleDescriptionForPc>
+          各種事業の立ち上げ、運営にかかわる調査・研究のサポートや、特定分野の専門家との提携のお手伝いなどを行います。
+        </S.ArticleDescriptionForPc>
+        <S.AnchorBtnForPc href="#" className="btn">
+          RESEARCH
+        </S.AnchorBtnForPc>
+      </S.ArticleForPc>
+    </S.DetailForPc>
   </S.ServiceForPc>
 );
--- src/presenter/component/Ch1/Service/styles.ts
+++ src/presenter/component/Ch1/Service/styles.ts
@@ -38,3 +38,108 @@ export const TitleForPc = styled.h2`
   color: ${s.colors.blue};
   display: inline-block;
 `;
+
+export const DetailForSp = styled.div`
+  margin-top: 53px;
+  color: ${s.colors.darkGray};
+`;
+
+export const ArticleForSp = styled.article`
+  &:not(:last-child) {
+    margin-bottom: 67px;
+  }
+`;
+
+export const ArticleTitleForSp = styled.h3`
+  margin-bottom: 17px;
+  font-size: 20px;
+  line-height: 1.75;
+`;
+
+export const ArticleDescriptionForSp = styled.p`
+  max-width: 264px;
+  margin: 0 auto 25px;
+  font-size: 12px;
+  line-height: 2;
+`;
+
+export const ArticleImgForSp = styled.figure`
+  height: 246px;
+  margin-bottom: 22px;
+  background-position: center;
+  background-size: cover;
+  background-image: url("/office.jpg");
+`;
+
+export const AnchorBtnForSp = styled.a`
+  display: block;
+  width: 208px;
+  margin: 0 auto;
+  padding: 11px 11px 13px;
+  box-sizing: border-box;
+  border: solid 2px currentColor;
+  border-radius: 24px;
+  color: ${s.colors.blue};
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+
+  :hover {
+    opacity: 0.75;
+  }
+`;
+
+export const DetailForPc = styled.div`
+  display: flex;
+  margin-top: 75px;
+  color: ${s.colors.darkGray};
+`;
+
+export const ArticleForPc = styled.article`
+  flex: 1;
+  margin-right: 3.22%;
+  :first-child {
+    margin-left: 3.22%;
+  }
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ArticleTitleForPc = styled.h3`
+  margin-bottom: 26px;
+  font-size: 30px;
+  line-height: 1.4;
+`;
+
+export const ArticleDescriptionForPc = styled.p`
+  margin: 0 auto 25px;
+  font-size: 12px;
+  line-height: 2;
+  width: 78%;
+`;
+
+export const ArticleImgForPc = styled.figure`
+  height: 360px;
+  margin-bottom: 33px;
+  background-position: center;
+  background-size: cover;
+  background-image: url("/office.jpg");
+`;
+
+export const AnchorBtnForPc = styled.a`
+  display: block;
+  width: 208px;
+  margin: 0 auto;
+  padding: 11px 11px 13px;
+  box-sizing: border-box;
+  border: solid 2px currentColor;
+  border-radius: 24px;
+  color: ${s.colors.blue};
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+  margin-top: auto;
+  :hover {
+    opacity: 0.75;
+  }
+`;
